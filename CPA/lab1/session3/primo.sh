32167d12ca3c976231b94fd56031b514dec3bf0e
jiwidi <fhjaime96@gmail.com>
2016-10-11 16:55:43 +0200

    lab3 completed cpa

---
+++ CPA/lab1/session3/primo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#PBS -l nodes=1,walltime=00:05:00
+#PBS -q cpa
+#PBS -d .
+
+OMP_NUM_THREADS=4 ./primoGrande
--- CPA/lab1/session3/primo_grande.c
+++ CPA/lab1/session3/primo_grande.c
@@ -2,44 +2,45 @@
 #include <math.h>
 #include <limits.h>
 #include <omp.h>
-#include <string.h>
-
-
-
 
 typedef unsigned long long Entero_grande;
 #define ENTERO_MAS_GRANDE  ULLONG_MAX
-
+double t1,t2;
 int primo(Entero_grande n)
 {
-  int volatile p,s,i;
-  
-
-   p = (n % 2 != 0 || n == 2);
+  int p;
+  Entero_grande i, s;
+  p = (n % 2 != 0 || n == 2);
 
   if (p) {
     s = sqrt(n);
+	t1 =omp_get_wtime();
+	#pragma omp parallel private(i)
+	{
+		int numberOfThreads = omp_get_num_threads();
+		int id=omp_get_thread_num();
 
-    for (i = 3; p && i <= s; i += 2)
-      if (n % i == 0) p = 0;
-  }
-
+    	for (i = id*2+3; p && i <= s; i += numberOfThreads)
+      		if (n % i == 0) p = 0;
+  		}
+	}
+	
+	printf("none\t%f\t%d\n", t2-t1, omp_get_num_threads());
   return p;
 }
 
 int main()
 {
-	int t1,t2;
   Entero_grande n;
-  t1 = omp_get_wtime();
+	
+	t1 =omp_get_wtime();
   for (n = ENTERO_MAS_GRANDE; !primo(n); n -= 2) {
     /* NADA */
   }
-
+t2=omp_get_wtime();
+printf("none\t%f\t%d\n", t2-t1, omp_get_num_threads());
   printf("El mayor primo que cabe en %d bytes es %llu.\n",
          sizeof(Entero_grande), n);
-	t2 = omp_get_wtime();
-  printf("none\t%f\t%d\n", t2 - t1, omp_get_num_threads());
 
   return 0;
 }
