44ae75bd7726d4e8a38095fbe30c3d0363cd49b8
jiwidi <fhjaime96@gmail.com>
2016-10-13 11:12:55 +0200

    kik

--- TSR/lab1/proxy/reverseProxy.js
+++ TSR/lab1/proxy/reverseProxy.js
@@ -1,5 +1,4 @@
 var net = require('net');
-
 var LOCAL_PORT = [8001,8002,8003,8004,8005];
 var LOCAL_IP = '127.0.0.1';
 var IP =['158.42.184.5','158.42.4.23','216.58.210.163','158.42.156.2','147.156.222.65']
@@ -14,6 +13,7 @@ var server = net.createServer(function(socket) {
         serviceSocket.on('data', function(data){socket.write(data);});
     });
 }).listen(LOCAL_PORT[port], LOCAL_IP);
+
 console.log("Redirecting connections to " + IP[port] + ":" + remotePort[port]);
 console.log("TCP server accepting connection on port: " + LOCAL_PORT[port]);
 
