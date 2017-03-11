556d698dc0da64846f2c66ae2a0e18e8f6557646
jiwidi <fhjaime96@gmail.com>
2017-03-11 20:42:06 +0100

    fix

--- SemesterB/SAR/lab3/SAR_p3_monkey_evolved.py
+++ SemesterB/SAR/lab3/SAR_p3_monkey_evolved.py
@@ -8,9 +8,14 @@ def load_object(file_name):
     with open(file_name, 'rb') as fh:
         obj = pickle.load(fh)
     return obj
-
+def generateSentences(diccionario,count):
+    return 0
+def generateSentence(diccionario,length):
+    return 0
 def main():
     diccionario=load_object(sys.argv[1])
+    keys=diccionario.keys()
+    generateSentences(diccionario,10)
 
 
 
