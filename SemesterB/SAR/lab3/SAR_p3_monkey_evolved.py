d71bffeca6cabff6ae55a5aff96a1a7b834aaa62
jiwidi <fhjaime96@gmail.com>
2017-03-12 03:27:53 +0100

    finish

--- SemesterB/SAR/lab3/SAR_p3_monkey_evolved.py
+++ SemesterB/SAR/lab3/SAR_p3_monkey_evolved.py
@@ -2,20 +2,34 @@ from collections import defaultdict
 from operator import itemgetter
 import sys
 import ast
+import random
 import pickle
 import re
 def load_object(file_name):
     with open(file_name, 'rb') as fh:
         obj = pickle.load(fh)
     return obj
-def generateSentences(diccionario,count):
-    return 0
-def generateSentence(diccionario,length):
-    return 0
+def generateSentences(diccionario,count,length):
+    output = open(sys.argv[2], 'w')
+    for m in range(count):
+        text='$ '
+        pal='$'
+        for n in range(length):
+            pal=generateWord(diccionario[pal][1])
+            text+=pal+' '
+            if(pal=='$'):
+                break
+        output.write(text+'\n')
+def generateWord(diccionario):
+    keys=diccionario.keys()
+    list=[]
+    for key in keys:
+        for n in range(diccionario[key]):
+            list.append(key)
+    return random.choice(list)
 def main():
     diccionario=load_object(sys.argv[1])
-    keys=diccionario.keys()
-    generateSentences(diccionario,10)
+    generateSentences(diccionario,10,25)
 
 
 
