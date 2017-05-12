fb6ec275e3e5d5994838629a3956b5469ec8cd4a
jiwidi <fhjaime96@gmail.com>
2017-05-13 00:11:01 +0200

    sar first indexer

--- SemesterB/SAR/Proyect/SAR_indexer.py
+++ SemesterB/SAR/Proyect/SAR_indexer.py
@@ -1,13 +1,35 @@
 import sys
 import os
 
+def procces(s):
+    nonAlphanumericalCharacters= ['!', '@' ,'#','&' ,'(', ')', '–', '[', '{', '}', ']', ':', ';', "'",',', '?' ,'/' ,'*','"','.']
+    for character in nonAlphanumericalCharacters:
+        s=s.replace(character, '')
+    s=s.lower()
+    return s
 
 def main():
-    #direc=sys.argv[1]
-    #finalName=sys.argv[2]
-    for filename in os.listdir("enero/"):
+    direc=sys.argv[1]
+    postingList={}
+    finalName=sys.argv[2]
+    for filename in os.listdir(direc):
         print(filename)
-        aux=open("enero/"+filename,'r')
-        #print(aux.read())
+        aux=open(direc+'/'+filename,'r')
+        raw=aux.read()
+        rawlist=raw.split('<DOC>')
+        for new in range(1,len(rawlist)):
+            Nid=rawlist[new][rawlist[new].find('<DOCID>')+len('<DOCID>'):rawlist[new].find('</DOCID>')]
+            Nrawtext=rawlist[new][rawlist[new].find('<TEXT>')+len('<TEXT>'):rawlist[new].find('</TEXT>')]
+            Ntext=procces(Nrawtext)
+            z=Ntext.split()
+            for w in z:
+                if w in list(postingList.keys()):
+                    postingList[w].append([Nid])
+                else:
+                    postingList[w]=[Nid]
         aux.close()
+        sys.exit()
+    for key in list(postingList.keys()):
+        print(key+'   '+str(postingList[key]))
+
 main()
